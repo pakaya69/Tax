2381411edebef44e2b1200ff00dc792ac746f783
Malintha <100715645+pakaya69@users.noreply.github.com>
2023-04-17 16:16:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
-function calculate() {
-  // Get the input value
-  let monthlyIncome = document.getElementById("monthly-income").value;
-
-  // Calculate the annual income
-  let annualIncome = monthlyIncome * 12;
-
-  // Calculate the tax expense based on the annual income
-  let taxExpense = 0;
-  if (annualIncome <= 1200000) {
-    taxExpense = 0;
-  } else if (annualIncome <= 1700000) {
-    taxExpense = (annualIncome - 1200000) * 0.06;
-  } else if (annualIncome <= 2200000) {
-    taxExpense = (annualIncome - 1700000) * 0.12 + 30000;
-  } else if (annualIncome <= 2700000) {
-    taxExpense = (annualIncome - 2200000) * 0.18 + 90000;
-  } else if (annualIncome <= 3200000) {
-    taxExpense = (annualIncome - 2700000) * 0.24 + 180000;
-  } else if (annualIncome <= 3700000) {
-    taxExpense = (annualIncome - 3200000) * 0.30 + 300000;
-  } else {
-    taxExpense = (annualIncome - 3700000) * 0.36 + 450000;
+function calculateTax() {
+  const monthlyIncome = Number(document.getElementById("income").value);
+  const annualIncome = monthlyIncome * 12;
+  let monthlyTax = 0;
+  
+  if (annualIncome > 1200000 && annualIncome <= 1700000) {
+    monthlyTax = ((annualIncome - 1200000) * 0.06 / 12).toFixed(2);
+  } else if (annualIncome > 1700000 && annualIncome <= 2200000) {
+    monthlyTax = (((annualIncome - 1700000) * 0.12) + 30000) / 12;
+  } else if (annualIncome > 2200000 && annualIncome <= 2700000) {
+    monthlyTax = (((annualIncome - 2200000) * 0.18) + 90000) / 12;
+  } else if (annualIncome > 2700000 && annualIncome <= 3200000) {
+    monthlyTax = (((annualIncome - 2700000) * 0.24) + 180000) / 12;
+  } else if (annualIncome > 3200000 && annualIncome <= 3700000) {
+    monthlyTax = (((annualIncome - 3200000) * 0.3) + 300000) / 12;
+  } else if (annualIncome > 3700000) {
+    monthlyTax = (((annualIncome - 3700000) * 0.36) + 450000) / 12;
   }
 
-  // Display the result
-  let result = document.getElementById("result");
-  result.innerHTML = "Monthly Tax Expense: " + taxExpense.toFixed(2);
+  document.getElementById("tax").value = monthlyTax.toFixed(2);
 }
